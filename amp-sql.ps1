9fa7f9dea3fb07134d5f732c7647d5caf1e9176c
unknown <whtecHko1000cJKxEE56@gmail.com>
2021-06-01 22:38:26 +0900

    first commit

---
+++ amp-sql.ps1
@@ -0,0 +1,45 @@
+﻿Set-PSDebug -Strict
+
+$sql = "INSERT INTO T_FOO(a, b, c) VALUES('?', 'foo', ?) ;"
+$parameters = @(120000, 250010)
+$total_record = 10000
+
+#################################################
+
+function create-sql($sql, $parameters) {
+    $regex = [regex]'\?'
+    
+    foreach ($p in $parameters) {
+        $sql = $regex.Replace($sql, $p, 1)
+    }
+
+    return $sql
+}
+
+function inc-param($parameterts) {
+    $end = $parameterts.count - 1
+    
+    foreach ($i in 0..$end) {
+        $parameterts[$i]++
+    }
+}
+
+#################################################
+
+$sqls = New-Object System.Text.StringBuilder
+
+foreach ($i in 0..$total_record) {
+
+    $s = create-sql $sql $parameters
+    
+    $sqls.AppendLine($s) | Out-Null
+    
+    if ($i % 1000 -eq 0) {
+        $sqls.ToString().TrimEnd()
+        $sqls.Clear() | Out-Null
+    }
+
+    inc-param $parameters
+}
+
+$sqls.ToString().TrimEnd()
